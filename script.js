704ca7a5674a3febcc470309bebd62b8539cd32f
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2022-03-18 16:55:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
 let vards = document.querySelector('.vards');
 
+
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 
-    fetch(API + '/sutit/'+ vards.value + '/' + zina.value)
+    fetch(API+'/sutit/'+vards.value+'/'+zina.value)
 }
 
 async function ieladetChataZinas()
@@ -20,33 +21,30 @@ async function ieladetChataZinas()
     zinas.innerHTML = dati;
 }
 
-//setInterval(ieladetChataZinas, 1000)
-
+//setInterval( ieladetChataZinas, 1000 )
 
 async function ieladetChataZinasJson()
 {
     let datiNoServera = await fetch(API + '/lasit');
     let dati = await datiNoServera.json();
     
-    //console.log(await dati[0]['zina'])
+    //console.log(await dati[0]['zina'] )
     zinas.innerHTML = '';
-
+    
     i = 0;
     while ( i < await dati.length )
     {
         //console.log(i);
-        let laiks = '[<i>' + '###         ' + '</i>']';
-        if ("laiks" in dati[i] {
-            let laiks = '[<i>' + dati[i]['laiks'] + '</i>] ';
+        let laiks = '[<i>' + '????          ' + '</i>] ';
+        if ("laiks" in dati[i]) {
+            laiks = '[<i>' + dati[i]['laiks'] + '</i>] ';
         }
-                
-        zinas.innerHTML = zinas.innerHTML + laiks + dati[i]['vards'] + ': ' + dati[i]['zina'] + '<br />';
-        i = i + 1;
+        zinas.innerHTML = zinas.innerHTML + laiks + dati[i]['vards']+': '+dati[i]['zina']+'<br />';
+
+        i = i+1;
     }
 
+    zinas.scrollTop = zinas.scrollHeight;
+}//beidzas ieladetChataZinasJson()
 
-    zinas.scrollTop = zinas.scrollHeight;    
-}//šeit beidzas funkcija ieladetChataZinasJson()
-
-
-setInterval(ieladetChataZinasJson, 1000)
+setInterval( ieladetChataZinasJson, 1000 )
