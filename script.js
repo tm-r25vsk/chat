d9349b0db9b4a63ba806fa3b9ee45aeb9f564647
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2022-03-18 15:22:14 +0200

    a

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const API = "https://chat22.tmuser.repl.co"
+
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards');
 
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
-
-
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+
+    fetch(API + '/sutit/'+ vards.value + '/' + zina.value)
 }
 
 async function ieladetChataZinas()
@@ -16,7 +18,6 @@ async function ieladetChataZinas()
     let datiNoServera = await fetch(API + '/lasit');
     let dati = await datiNoServera.text();
     zinas.innerHTML = dati;
-
 }
 
 setInterval(ieladetChataZinas, 1000)
