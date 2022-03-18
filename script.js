501bf4cd43c5821d19d9ed030b1f015076996752
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2022-03-18 16:51:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,15 @@ async function ieladetChataZinasJson()
     while ( i < await dati.length )
     {
         //console.log(i);
-        zinas.innerHTML = zinas.innerHTML + dati[i]['vards'] + ': ' + dati[i]['zina'] + '<br />';
+        if ("laiks" in dati[i] {
+            let laiks = '[<i>' + dati[i]['laiks'] + '</i>] ';
+        }
+        else {
+            let laiks = '[<i>' + '###         ' + '</i>']'
+        
+        }
+        
+        zinas.innerHTML = zinas.innerHTML + laiks + dati[i]['vards'] + ': ' + dati[i]['zina'] + '<br />';
         i = i + 1;
     }
 
