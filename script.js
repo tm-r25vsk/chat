660912381d1e5db052bc1c9e1d89cefb74c4dd38
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2022-03-18 12:29:36 +0200

    a

---
+++ script.js
@@ -0,0 +1,22 @@
+const API = "https://chat22.tmuser.repl.co"
+let zina = document.querySelector('.manaZina');
+let zinas = document.querySelector('.chataZinas');
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+
+
+
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
+}
+
+async function ieladetChataZinas()
+{
+    let datiNoServera = await fetch(API + '/lasit');
+    let dati = await datiNoServera.text();
+    zinas.innerHTML = dati;
+
+}
+
+setInterval(ieladetChataZinas, 1000)
