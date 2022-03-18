63cf454581e100cfc691fd0a373f77e2a8b621bb
tm-r25vsk <83770529+tm-r25vsk@users.noreply.github.com>
2022-03-18 15:56:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,8 +34,14 @@ async function ieladetChataZinasJson()
     i = 0;
     while ( i < await dati.length )
     {
-        console.log(i);
+        //console.log(i);
         zinas.innerHTML = zinas.innerHTML + dati[i]['vards'] + ': ' + dati[i]['zina'] + '<br />';
         i = i + 1;
     }
+
+
+    zinas.scrollTop = zinas.scrollHeight;    
 }//šeit beidzas funkcija ieladetChataZinasJson()
+
+
+setInterval(ieladetChataZinasJson, 1000)
